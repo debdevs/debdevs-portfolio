7a9f890ad23df048610bceabd7da68e3ab7a8434
debdevs <debdevs97@gmail.com>
2022-10-11 04:09:02 -0400

    add interactivity to LandingTabs component
    added state management to landing tabs component. Tabs now stick when clicked.

--- debdevs-portfolio/components/UI/atoms/LandingToggleButton/LandingToggleButton.js
+++ debdevs-portfolio/components/UI/atoms/LandingToggleButton/LandingToggleButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import styles from './LandingToggleButton.module.css';
 
-const LandingToggleButton = () => {
+const LandingToggleButton = ({ title }) => {
   return (
     <div>
       <max_container className={styles.max_container}>
@@ -11,7 +11,7 @@ const LandingToggleButton = () => {
         <button_parent className={styles.button_parent}>
           <div className={styles.rainbowGradient}>
             <div className={styles.innerSquare}>
-              <h1 className={styles.h1}>Overview</h1>
+              <h1 className={styles.h1}>{title}</h1>
             </div>
           </div>
         </button_parent>
--- debdevs-portfolio/components/UI/molecules/LandingTabs/LandingTabs.js
+++ debdevs-portfolio/components/UI/molecules/LandingTabs/LandingTabs.js
@@ -3,21 +3,45 @@ import styles from './LandingTabs.module.css';
 import React from 'react';
 import { useState } from 'react';
 const options = ['Overview', 'Challenges', 'Github', 'Site'];
+
 const LandingTabs = () => {
   const [active, setActive] = useState(options[0]);
+  const [count, setCount] = useState(0);
+  const [clickedId, setClickedId] = useState(-1);
+  const clickStyles = {
+    h1_color: 'rgba(191, 209, 217, 1)',
+    RainbowGradient_background_color: 'white',
+    RainbowGradient_transform: 'translateY(5px)',
+  };
   return (
     <tabs className={styles.tabs}>
-      {options.map((option) => (
-        <LandingToggleButton
-          key={option}
-          active={active === option}
-          onClick={() => setActive(option)}
+      {options.map((option, i) => (
+        <max_container
+          className={styles.max_container}
+          onClick={() => setClickedId(i)}
         >
-          {option}
-        </LandingToggleButton>
+          <blue_bg className={styles.blue_bg}></blue_bg>
+          <white_bg className={styles.white_bg}></white_bg>
+          <button_parent className={styles.button_parent}>
+            <div
+              className={
+                i === clickedId
+                  ? styles.active_rainbowGradient
+                  : styles.rainbowGradient
+              }
+            >
+              <div
+                key={i}
+                className={
+                  i === clickedId ? styles.active_text : styles.innerSquare
+                }
+              >
+                <h1>{option}</h1>
+              </div>
+            </div>
+          </button_parent>
+        </max_container>
       ))}
-
-      <LandingToggleButton />
     </tabs>
   );
 };
