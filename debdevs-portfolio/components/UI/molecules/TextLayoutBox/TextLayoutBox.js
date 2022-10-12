2df250a8cc965bd1452b55c9d9bd7fab9517b9ab
debdevs <debdevs97@gmail.com>
2022-10-12 08:33:49 -0400

    update text layout box components
    revised text layout component and added data to project data file. Added line breaks to description text

--- debdevs-portfolio/components/UI/molecules/TextLayoutBox/TextLayoutBox.js
+++ debdevs-portfolio/components/UI/molecules/TextLayoutBox/TextLayoutBox.js
@@ -3,8 +3,10 @@ import styles from './TextLayoutBox.module.css';
 const TextLayoutBox = ({ header_text, paragraph_text }) => {
   return (
     <div className={styles.text_layout_box}>
-      <h1>{header_text}</h1>
-      <h2>{paragraph_text}</h2>
+      <div className={styles.text_layout_box_container}>
+        <h1>{header_text}</h1>
+        <h2>{paragraph_text}</h2>
+      </div>
     </div>
   );
 };
