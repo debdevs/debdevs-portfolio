3151e9e41375ae47e80e2d19dffd96aadce9db3f
debdevs <debdevs97@gmail.com>
2022-11-06 06:32:32 -0500

    create portfolio page
    create portfolio page hero and base components. Added size styling for porfolio components to globals.

---
+++ debdevs-portfolio/components/UI/templates/PortfolioHero/PortfolioHero.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './PortfolioHero.module.css';
+import IconWithToolTip from '../../atoms/IconWithToolTip/IconWithToolTip';
+import skills from '../../../../public/data/skills.js';
+import { motion } from 'framer-motion';
+import PortfolioTabs from '../../molecules/PortfolioTabs/PortfolioTabs';
+const PortfolioHero = () => {
+  return (
+    <section className={styles.portfolio_header}>
+      <content_box className={styles.content_box}>
+        <left_items className={styles.left_items}>
+          <portfolio_header_info_box
+            className={styles.portfolio_header_info_box}
+          >
+            <h1 className={styles.h1}>Welcome</h1>
+            <h2 className={styles.h2}>
+              To my <span className={styles.span}>Portfolio</span>
+            </h2>
+            <p className={styles.p}>
+              From web designs to visuals, I’ve got projects
+            </p>
+            <line_div className={styles.line_div}></line_div>
+            <button_array className={styles.button_array}>
+              <button className={styles.blue_button}>About</button>
+              <button className={styles.white_button}>
+                <div className={styles.white_inner_button}>My Work</div>
+              </button>
+            </button_array>
+            <tabs_array className={styles.tabs_array}>
+              <PortfolioTabs />
+            </tabs_array>
+          </portfolio_header_info_box>
+        </left_items>
+      </content_box>
+    </section>
+  );
+};
+
+export default PortfolioHero;
---
+++ debdevs-portfolio/pages/portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PortfolioHero from '../components/UI/templates/PortfolioHero/PortfolioHero';
+import NavBar from '../components/UI/templates/NavBar/NavBar';
+
+const portfolio = () => {
+  return (
+    <div>
+      <NavBar />
+      <PortfolioHero />
+    </div>
+  );
+};
+
+export default portfolio;
