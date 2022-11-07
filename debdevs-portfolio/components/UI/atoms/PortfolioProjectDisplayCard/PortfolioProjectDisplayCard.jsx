f4d2bb8d78e367a2d95371dbfaf869e83a0f7bbc
debdevs <debdevs97@gmail.com>
2022-11-07 04:04:10 -0500

    add animations to PortfolioProjectDisplayCard

--- debdevs-portfolio/components/UI/atoms/PortfolioProjectDisplayCard/PortfolioProjectDisplayCard.jsx
+++ debdevs-portfolio/components/UI/atoms/PortfolioProjectDisplayCard/PortfolioProjectDisplayCard.jsx
@@ -3,20 +3,29 @@ import styles from './PortfolioProjectDisplayCard.module.css';
 import Image from 'next/future/image';
 import skyway_web_image from '../../../../public/images/SkywayWebsite.png';
 import PortfolioProjectButton from '../PortfolioProjectButton/PortfolioProjectButton';
-const PortfolioProjectDisplayCard = ({ button_text }) => {
+import { motion, AnimatePresence } from 'framer-motion';
+const PortfolioProjectDisplayCard = ({ button_text, key_value = 0 }) => {
   return (
-    <div className={styles.portfolio_card_container}>
-      <div className={styles.portfolio_image_container}>
-        <Image
-          className={styles.image_display_base_image}
-          src={skyway_web_image}
-          alt=""
-          objectFit="cover"
-          quality={100}
-        />
-      </div>
-      <PortfolioProjectButton button_text={button_text} />
-    </div>
+    <AnimatePresence>
+      <motion.div
+        key={key_value}
+        initial={{ opacity: 0, traslateX: 0, translateY: 50 }}
+        animate={{ opacity: 1, traslateX: 0, translateY: 0 }}
+        transition={{ duration: 0.7, delay: 0 + key_value * 0.25 }}
+        className={styles.portfolio_card_container}
+      >
+        <div className={styles.portfolio_image_container}>
+          <Image
+            className={styles.image_display_base_image}
+            src={skyway_web_image}
+            alt=""
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        <PortfolioProjectButton button_text={button_text} />
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
--- debdevs-portfolio/components/UI/templates/PortfolioSubheading/PortfolioSubheading.jsx
+++ debdevs-portfolio/components/UI/templates/PortfolioSubheading/PortfolioSubheading.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ProjectsDisplayCard from '../../molecules/ProjectDisplayCard/ProjectDisplayCard';
 import styles from './PortfolioSubheading.module.css';
 import PortfolioProjectDisplayCard from '../../atoms/PortfolioProjectDisplayCard/PortfolioProjectDisplayCard';
+import data from '../../../../public/project_data.js';
+
 const PortfolioSubheading = () => {
   return (
     <div className={styles.portfolio_subheading_parent}>
       <div className={styles.portfolio_subheading_cards}>
-        <PortfolioProjectDisplayCard />
-        <PortfolioProjectDisplayCard />
-        <PortfolioProjectDisplayCard />
+        {data.projects.map((name, key) => (
+          <PortfolioProjectDisplayCard key_value={key} />
+        ))}
       </div>
     </div>
   );
