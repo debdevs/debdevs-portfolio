3bd971b49c27ed0a1648b951be26dee63dfcf118
debdevs <debdevs97@gmail.com>
2022-11-23 20:03:44 -0500

    create components for about pages
    created and imported components for about page

--- debdevs-portfolio/components/UI/templates/AboutHero/AboutHero.js
+++ debdevs-portfolio/components/UI/templates/AboutHero/AboutHero.js
@@ -9,30 +9,41 @@ import PortfolioTabs from '../../molecules/PortfolioTabs/PortfolioTabs';
 const AboutHero = () => {
   return (
     <section className={styles.about_header}>
-      <content_box className={styles.content_box}>
+      
         
-        <left_items className={styles.left_items}>
+     
           <about_header_info_box
             className={styles.about_header_info_box}
           >
-            <h1 className={styles.h1}>About Me</h1>
+            <top_texts className={styles.top_texts}>
+            <h1 className={styles.h1}>About Me:</h1>
             <h2 className={styles.h2}>
-              I'm a <span className={styles.span}>Developer</span>
-            </h2>
-            <p className={styles.p}>
-              I'm a web developer who's passionate about creating engaging visual experiences, and translating designs into code. <br></br> <br></br> Over the years, I've worked as an animator who's designed custom python scripts and business solutions for various companies
+              I'm a <span className={styles.span}>developer</span>
+            </h2></top_texts>
+            <p className={styles.p_header}>
+            With a passion for visuals and a drive to get it done
+
+
             </p>
             <line_div className={styles.line_div}></line_div>
-            <button_array className={styles.button_array}>
-             
+            <p className={styles.p}>
+              I'm passionate about creating engaging visual experiences, and translating designs into code. 
+              <br></br> 
+              <br></br> 
+              Over the years, I've worked as an animator who's designed custom python scripts and business solutions for various companies.  <br></br> <br></br> 
 
-            </button_array>
-            <tabs_array className={styles.tabs_array}>
-             
-            </tabs_array>
+I’ve also worked to create custom designs and more efficient workflows. <br></br> <br></br> 
+
+I’m currently looking to get practical experience while working towards a computer science degree.<br></br> <br></br> 
+
+Take a look at my previous work below
+
+            </p>
+   
+  
           </about_header_info_box>
-        </left_items>
-      </content_box>
+     
+    
     </section>
   );
 };
--- debdevs-portfolio/pages/about.js
+++ debdevs-portfolio/pages/about.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import AboutHero from '../components/UI/templates/AboutHero/AboutHero'
+import AboutSubheading from '../components/UI/templates/AboutSubheading/AboutSubheading'
 import NavBar from '../components/UI/templates/NavBar/NavBar'
 const about = () => {
   return (
-    <div><NavBar/><AboutHero/></div>
+    <div>
+      <NavBar/>
+      <AboutHero/>
+      <AboutSubheading/>
+    </div>
   )
 }
 
