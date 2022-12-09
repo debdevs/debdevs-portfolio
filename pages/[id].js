c0b821d2faf688475606a26bc5d11114122e034f
debdevs <debdevs97@gmail.com>
2022-12-09 07:15:33 -0500

    test dynamic sanity data import

--- pages/[id].js
+++ pages/[id].js
@@ -1,11 +1,24 @@
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
 
-import { client } from 'client'
+import data from '../public/project_data'
+import NextLink from 'next/link';
+import ProjectsHero from '../components/UI/templates/ProjectsHero/ProjectsHero';
+import NavBar from '../components/UI/templates/NavBar/NavBar';
+import ProjectsSubheading from '../components/UI/templates/ProjectsSubheading/ProjectsSubheading';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { urlFor, client } from 'client';
 
-function urlFor (source) {
-  return imageUrlBuilder(client).image(source)
-}
+
+const variants = {
+    hidden: { opacity: 0, x: 0, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 0 },
+    duration: { opacity: 0, x: 0, y: 0 },
+    transition: { ease: 'easeInOut', duration: 20 },
+  };
+
 
 const ptComponents = {
   types: {
@@ -28,37 +41,33 @@ const Post = ({projects}) => {
   const {
 
     name = 'Missing name',
+    id = projects[1].id,
     categories,
     authorImage,
     body = []
   } = projects
   return (
-    <article>
-      <h1>{name}</h1>
-      <span>By {name}</span>
-      {categories && (
-        <ul>
-          Posted in
-          {categories?.map(category => <li key={category}>{category}</li>)}
-        </ul>
-      )}
-      {authorImage && (
-        <div>
-          <img
-            src={urlFor(authorImage)
-              .width(50)
-              .url()}
-            alt={`${name}'s picture`}
-          />
-        </div>
-      )}
+    <div>
 
-    </article>
+    <motion.main
+      variants={variants} // Pass the variant object into Framer Motion
+      initial="hidden" // Set the initial state to variants.hidden
+      animate="enter" // Animated state to variants.enter
+      exit="exit" // Exit state (used later) to variants.exit
+      transition={{ type: 'linear' }} // Set the transition to linear
+      className=""
+    >
+      <NavBar />
+      <ProjectsHero projectId={id}/>
+      <ProjectsSubheading projectId={id}/>
+    </motion.main>
+  </div>
   )
 }
 
 const query = groq`*[_type == "projects"]`
 export async function getStaticPaths() {
+   
   const paths = await client?.fetch(query).then(
     groq`*[_type == "projects"]`
   )
@@ -67,7 +76,7 @@ export async function getStaticPaths() {
 
 
   return {
-    paths: paths?.map((link) => ({params: {link}}, {params: { id: "showman-video" }})),
+    paths: paths?.map((link) => ({params: {link}}, {params: { id: "rocketeer"}})),
     fallback: true,
     
   }
